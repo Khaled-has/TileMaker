bc2e86ecd84d86feaf3f6dc06877af835997e58c
shc <shc@DESKTOP-K2LCDN4>
2025-11-28 15:04:39 +0300

    Fixe some WIN32 components

--- Editor/src/Core/Platform/Windows/WindowsWindow.cpp
+++ Editor/src/Core/Platform/Windows/WindowsWindow.cpp
@@ -1,9 +1,12 @@
-#include "WindowsWindow.h"
+﻿#include "WindowsWindow.h"
 
 #include "Log/Log.h"
 #include "WindowsInput.h"
 
 #include <imgui_impl_sdl3.h>
+
+#include <SDL3/SDL_system.h>
+#include <Windows.h>
 
 #ifndef ASSETS_PATH
 #endif
@@ -34,7 +37,6 @@ namespace Editor {
 
 		while (SDL_PollEvent(&ev))
 		{
-			
 			//ImGui Event
 			ImGui_ImplSDL3_ProcessEvent(&ev);
 
@@ -113,7 +115,12 @@ namespace Editor {
 		SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
 		SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 24);
 
-		w_Window = SDL_CreateWindow(w_Prop.Title.c_str(), w_Prop.Width, w_Prop.Height, SDL_WINDOW_OPENGL);
+		w_Window = SDL_CreateWindow(
+			w_Prop.Title.c_str(), 
+			w_Prop.Width, w_Prop.Height, 
+			SDL_WINDOW_OPENGL | SDL_WINDOW_RESIZABLE
+		);
+
 		ED_ASSERT(w_Window, "Failed create SDL window!");
 
 		w_Context = SDL_GL_CreateContext(w_Window);
@@ -123,12 +130,60 @@ namespace Editor {
 
 		SDL_GL_SetSwapInterval(true);
 
+
+		// Some window details ( WIN32 )
+		SDL_PropertiesID props = SDL_GetWindowProperties(w_Window);
+
+		HWND hwnd = (HWND)SDL_GetPointerProperty(
+			props,
+			SDL_PROP_WINDOW_WIN32_HWND_POINTER,
+			NULL
+		);
+
+		// Update window's size for fixe some ui components
+		SDL_SetWindowSize(w_Window, w_Prop.Width, w_Prop.Height - 10);
+		SDL_SetWindowSize(w_Window, w_Prop.Width, w_Prop.Height);
+
+		ED_ASSERT(hwnd, "Failed to take HWND WIN32 for SDL3 window");
+
+		LONG style = GetWindowLong(hwnd, GWL_STYLE);
+
+		style &= ~WS_CAPTION;
+		style &= ~WS_SYSMENU;
+		style &= ~WS_MAXIMIZEBOX;
+		style &= ~WS_MINIMIZEBOX;
+
+		SetWindowLong(hwnd, GWL_STYLE, style);
+
+		SetWindowPos(
+			hwnd, NULL,
+			0, 0, 0, 0,
+			SWP_NOMOVE | SWP_NOSIZE | SWP_NOZORDER | SWP_FRAMECHANGED
+			);
+
 		ED_LOG_TRACE("=========>  Windows window created!");
 	}
 
 	WindowsWindow* CreateWindowsWindow()
 	{
 		return new WindowsWindow();
+	}
+
+	void handleWindowSize(SDL_Window* win, SDL_Event* ev, unsigned int side)
+	{
+
+		int w, h, x, y;
+		SDL_GetWindowSize(win, &w, &h);
+
+		if (ev->type == SDL_EVENT_MOUSE_BUTTON_DOWN) {
+
+			// Right
+			if (side == 0)
+			{
+				SDL_SetWindowSize(win, w + ev->motion.xrel, h);
+				ED_LOG_TRACE("{0} | {1}", ev->motion.xrel, ev->motion.yrel);
+			}
+		}
 	}
 
 }
